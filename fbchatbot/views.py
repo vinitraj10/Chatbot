8fe5db3f4820b6b028614822d02fa001d64c8f6a
vinitraj10 <ent.vinit01@gmail.com>
2017-07-14 14:03:10 +0530

    Deployed on herokugit add .

--- fbchatbot/views.py
+++ fbchatbot/views.py
@@ -1,15 +1,15 @@
-import json,requests
+import json,requests,datetime
 from pprint import pprint
 from wit import Wit
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
-wit_access_token = "WEJRB35U2WDNGJNX5OFNYGBDJSGPFLXR"
+wit_access_token = <wit_access_token>
 @csrf_exempt
 def fbchat(request):
 	if request.method == 'GET':
-		if request.GET['hub.verify_token'] == '3425896715':
+		if request.GET['hub.verify_token'] == <verify_token>:
 			return HttpResponse(request.GET['hub.challenge'])
 		else:
 			return HttpResponse("Error,invalid token")
@@ -44,13 +44,13 @@ def fbchat(request):
 
 
 def master_post_fb(fb_id,text):
-	post_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAXsQKdeWhYBANEnYdJxrkGclTfc7MaZCY4H6qrEoPttwn2t73zF3i6SsN5Q3FweR1w1v6QqyFVmchEWwnSHgELwKx70HTAulZAmEBG1rE2hsz0gtwqcZBCVhnsl2CdQz1jjn8kUmZBeUBj1I9bPimAlZBUzON9rvKcakaglp0AZDZD'
+	post_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=<access_token>'
 	reply_msg = json.dumps({"recipient":{"id":fb_id}, "message":{"text":text}})
 	requests.post(post_url,headers={"Content-Type":"application/json"},data=reply_msg)
 
 def greet_post_fb(fb_id,text):
-	post_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAXsQKdeWhYBANEnYdJxrkGclTfc7MaZCY4H6qrEoPttwn2t73zF3i6SsN5Q3FweR1w1v6QqyFVmchEWwnSHgELwKx70HTAulZAmEBG1rE2hsz0gtwqcZBCVhnsl2CdQz1jjn8kUmZBeUBj1I9bPimAlZBUzON9rvKcakaglp0AZDZD'
-	reply_msg = json.dumps({
+	post_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=<access_token>'
+		reply_msg = json.dumps({
 		"recipient":{"id":fb_id}, 
 		"message":{
 			"text":text,
@@ -64,12 +64,12 @@ def greet_post_fb(fb_id,text):
  )
 	requests.post(post_url,headers={"Content-Type":"application/json"},data=reply_msg)
 
-def post_weather_fb(weather_report,weather_icon,fb_id):
-	post_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAXsQKdeWhYBANEnYdJxrkGclTfc7MaZCY4H6qrEoPttwn2t73zF3i6SsN5Q3FweR1w1v6QqyFVmchEWwnSHgELwKx70HTAulZAmEBG1rE2hsz0gtwqcZBCVhnsl2CdQz1jjn8kUmZBeUBj1I9bPimAlZBUzON9rvKcakaglp0AZDZD'
-	reply_msg = json.dumps({
+def post_weather_fb(final_report,fb_id):
+	post_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=<access_token>'
+		reply_msg = json.dumps({
 		"recipient":{"id":fb_id},
 		"message":{
-			"text":weather_report,
+			"text":final_report,
 			}
 		}
 	)
@@ -77,8 +77,8 @@ def post_weather_fb(weather_report,weather_icon,fb_id):
 
 
 def askLocation(fb_id):
-	post_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAXsQKdeWhYBANEnYdJxrkGclTfc7MaZCY4H6qrEoPttwn2t73zF3i6SsN5Q3FweR1w1v6QqyFVmchEWwnSHgELwKx70HTAulZAmEBG1rE2hsz0gtwqcZBCVhnsl2CdQz1jjn8kUmZBeUBj1I9bPimAlZBUzON9rvKcakaglp0AZDZD'
-	reply_msg = json.dumps({
+	post_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=<access_token>'
+		reply_msg = json.dumps({
 		"recipient":{"id":fb_id}, 
 		"message":{
 			"text":'Please Share Your Location',
@@ -95,12 +95,18 @@ def askLocation(fb_id):
 
 def getWeather(lat,lon,fb_id):
 	appid='b1b15e88fa797225412429c1c50c122a1'
-	url='http://samples.openweathermap.org/data/2.5/weather?lat=' + str(lat) + "&" + "lon=" + str(lon) + "&appid=" + str(appid) 
+	key="772973e281f73ebc401d23c974de0f09"
+	url="https://api.darksky.net/forecast/"+str(key)+"/"+ str(lat) + "," + str(lon)
+	#pprint(requests.get(url).json())
 	result=requests.get(url).json()
-	weather_report=result['weather'][0]['description']
-	weather_icon = result['weather'][0]['icon']
-	print(weather_icon)
-	post_weather_fb(weather_report,weather_icon,fb_id)
+	temp_max=int(((result['daily']['data'][0]['temperatureMax'])-32)*0.5555555555555556)
+	temp_min=int(((result['daily']['data'][0]['temperatureMin'])-32)*0.5555555555555556)
+	sunrise=datetime.datetime.fromtimestamp(int(result['daily']['data'][0]['sunriseTime'])).strftime('%H:%M:%S')
+	sunset=datetime.datetime.fromtimestamp(int(result['daily']['data'][0]['sunsetTime'])).strftime('%H:%M:%S')
+	timeZone=(result['timezone'])
+	summary=result['hourly']['summary']
+	final_report="Max temp : " + str(temp_max) + " °C \n" + "Min temp : " + str(temp_min) + " ° C \n" +"Sunrise time : " + str(sunrise) + "\n" +"Sunset time : " +str(sunset) + "\n" + "summary : " + str(summary)
+	post_weather_fb(final_report,fb_id)
 
 def cleaning_text(text):
 	fresh_text=[]
@@ -112,15 +118,21 @@ def cleaning_text(text):
 def send(request,response):
 	fb_id=request['session_id']
 	#pprint(request)
+	user_details_url = "https://graph.facebook.com/v2.6/%s"%fb_id
+	user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':'<access_token>'}
+	user_details = requests.get(user_details_url, user_details_params).json()
+	pprint(user_details)
 	if request['entities']:
 		text=str(response['text'])
 		text=cleaning_text(text)
 		intent_type= request['entities']['intent'][0]['value']      #Storing intent type for user interactions for different intents.
 		if intent_type == 'greet':
+			text="Hey " + str(user_details['first_name']) + " This is my beta version,I can only find weather condition,but soon I will try to learn more things to become more useful for you :-) :)"
 			greet_post_fb(fb_id,text)
 		elif intent_type=='weather':
-			text="santa banta hahahahahaha"	
 			askLocation(fb_id)
+		else:
+			master_post_fb(fb_id,text)
 	else:
 		default_text="Sorry I don't Understand please retry some other value!! :("
 		master_post_fb(fb_id,default_text)
